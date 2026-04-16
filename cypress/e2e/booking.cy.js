05af18acf6fd695259e6fac501b9db882f627054
Muhammad Fikri <23225181@adtec.edu.my>
2026-04-16 10:31:29 +0800

    uabh css

--- cypress/e2e/booking.cy.js
+++ cypress/e2e/booking.cy.js
@@ -1,23 +1,45 @@
 describe('Sistem Pengurusan Produk', () => {
-    
-      it('Ujian Paparan', () => {
-        cy.visit('http://127.0.0.1:5500/index.html');
-        cy.contains('Sistem Pengurusan Produk');
-      });
-    
-      it('Ujian Borang', () => {
-        cy.visit('http://127.0.0.1:5500/index.html');
-    
-        cy.get('#name').type('Laptop');
-        cy.get('#category').type('Elektronik');
-        cy.get('#price').type('3000');
-        cy.get('button').click();
-      });
-    
-      it('Ujian Hapus', () => {
-        cy.visit('http://127.0.0.1:5500/index.html');
-    
-        cy.contains('Hapus').click();
-      });
-    
+
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+  });
+
+  it('Ujian Paparan', () => {
+    cy.contains('Sistem Pengurusan Produk').should('exist');
+    cy.get('table').should('exist');
+  });
+
+  it('Ujian Borang', () => {
+
+    cy.get('#name').type('Laptop');
+    cy.get('#category').type('Elektronik');
+    cy.get('#price').type('3000');
+
+    cy.get('form button').click();
+
+    // CHECK DATA MASUK TABLE
+    cy.contains('Laptop').should('exist');
+    cy.contains('Elektronik').should('exist');
+    cy.contains('3000').should('exist');
+  });
+
+ it('Ujian Hapus', () => {
+
+  cy.get('#name').type('Phone');
+  cy.get('#category').type('Gadget');
+  cy.get('#price').type('2000');
+
+  cy.get('form button').click();
+
+  // DELETE ROW YANG BETUL
+  cy.contains('Phone')
+    .parent()
+    .within(() => {
+      cy.contains('Hapus').click();
     });
+
+  // VERIFY HILANG
+  cy.get('body').should('not.contain', 'Phone');
+});
+
+});
